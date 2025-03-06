d283f22405425edf7c4fa022752fb5c080572e83
mflkee <makeevgleb86rus@gmail.com>
2025-03-06 16:44:49 +0500

    add polybar

--- dot_config/polybar/scripts/executable_ssd_stat.sh
+++ dot_config/polybar/scripts/executable_ssd_stat.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Получаем использование оперативной памяти (в процентах)
-memory_usage=$(free | awk '/^Mem:/ {print $3/$2 * 100}' | awk '{printf "%d\n", $1}')
+# Получаем свободное место на диске (в гигабайтах)
+disk_free=$(df -h / | awk 'NR==2 {print $4}')
+
+# Получаем процент использования диска (для цветовой индикации)
+disk_usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//g')
 
 # Определение цветов
-COLOR1="#50FA7B" # до 50% (зеленый)
+COLOR1="#50FA7B" # до 50% использования (зеленый)
 COLOR2="#FFB86C" # от 50% до 80% (оранжевый)
 COLOR3="#FF5555" # выше 80% (красный)
 
-# Выбор цвета в зависимости от использования памяти
-if [[ "$memory_usage" -ge 80 ]]; then
+# Выбор цвета в зависимости от использования диска
+if [[ "$disk_usage" -ge 80 ]]; then
   COLOR=$COLOR3
-elif [[ "$memory_usage" -ge 50 ]]; then
+elif [[ "$disk_usage" -ge 50 ]]; then
   COLOR=$COLOR2
 else
   COLOR=$COLOR1
 fi
 
-# Иконка из Hack Nerd Font
-ICON=""
+# Иконка из Nerd Fonts (󰋊 — диск)
+ICON="󰋊"
 
-# Вывод в формате Polybar
-echo "%{T3}%{F$COLOR}$ICON%{T-}%{F-} %{F$COLOR}${memory_usage}%%{F-}"
+# Вывод в формате Polybar: свободное место + цвет по использованию
+echo "%{T3}%{F$COLOR}$ICON%{T-}%{F-} %{F$COLOR}${disk_free}%{F-}"
