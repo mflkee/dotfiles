4617c57e89d5fbf645f62a47919527292a9fd719
mflkee <makeevgleb86rus@gmail.com>
2025-03-10 11:37:33 +0500

    <-kitty

--- dot_config/wezterm/empty_wezterm.lua
+++ dot_config/wezterm/empty_wezterm.lua
@@ -1,5 +1,11 @@
 local wezterm = require 'wezterm'
 
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
+
 return {
   set_environment_variables = {
     TERM = "xterm-256color",
