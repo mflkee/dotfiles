b298d4556fea72c44752f60bdccfe738df590cd9
mflkee <makeevgleb86rus@gmail.com>
2025-06-13 17:53:28 +0500

    vpn-upd

--- dot_config/polybar/scripts/executable_openvpn_menu.sh
+++ dot_config/polybar/scripts/executable_openvpn_menu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CONFIG_DIR="$HOME/.config/openvpn/configs/"
-CURRENT_LINK="$HOME/.config/openvpn/client.conf"
 VPN_NAME="OpenVPN Connection"
 
 # Получаем список доступных .ovpn файлов
@@ -10,23 +9,19 @@ SELECTED=$(echo "$CONFIGS" | rofi -dmenu -p "Выберите VPN файл:")
 
 if [ -n "$SELECTED" ]; then
     NEW_CONFIG="$CONFIG_DIR$SELECTED"
-    BASE_NAME="${SELECTED%.ovpn}"  # Убираем расширение .ovpn
-    
-    # Удаляем старое подключение, если оно существует
+    BASE_NAME="${SELECTED%.ovpn}"
+
+    # Удаляем старое подключение
     nmcli connection delete "$BASE_NAME" 2>/dev/null || true
     nmcli connection delete "$VPN_NAME" 2>/dev/null || true
-    
+
     # Импортируем новый .ovpn файл
     if nmcli connection import type openvpn file "$NEW_CONFIG"; then
-        # Переименовываем подключение в фиксированное имя
-        if nmcli connection modify "$BASE_NAME" connection.id "$VPN_NAME"; then
-            notify-send "OpenVPN" "Выбран файл: $SELECTED. Подключитесь через Polybar." -t 2000
-        else
-            notify-send "OpenVPN" "Ошибка при переименовании подключения." -t 2000
-        fi
+        nmcli connection modify "$BASE_NAME" connection.id "$VPN_NAME"
+        notify-send "OpenVPN" "Файл $SELECTED загружен. Подключайтесь." -t 2000
     else
-        notify-send "OpenVPN" "Ошибка при импорте файла: $SELECTED" -t 2000
+        notify-send "OpenVPN" "Ошибка импорта файла $SELECTED" -t 2000
     fi
 else
-    notify-send "OpenVPN" "Выбор отменен" -t 2000
+    notify-send "OpenVPN" "Выбор отменён" -t 2000
 fi
--- dot_config/polybar/scripts/executable_vpn-status.sh
+++ dot_config/polybar/scripts/executable_vpn-status.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 VPN_NAME="OpenVPN Connection"
-active="%{F#8BE9FD}%{T12}VPN%{F-}"
-disabled="%{F#FF5555}%{T12}VPN%{F-}"
+
+active="%{F#8BE9FD}%{T11}VPN%{F-}"
+disabled="%{F#FF5555}%{T11}VPN%{F-}"
 
 if nmcli connection show --active | grep -q "$VPN_NAME"; then
     echo "$active"
