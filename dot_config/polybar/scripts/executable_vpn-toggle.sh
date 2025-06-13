1fefe78202c1b0c9c5cd8fed1d003bd5023a7001
mflkee <makeevgleb86rus@gmail.com>
2025-06-13 12:07:27 +0500

    vpn-toggle

--- dot_config/polybar/scripts/executable_vpn-toggle.sh
+++ dot_config/polybar/scripts/executable_vpn-toggle.sh
@@ -3,11 +3,9 @@
 VPN_NAME="OpenVPN Connection"
 
 if nmcli connection show --active | grep -q "$VPN_NAME"; then
-    echo "$(date): VPN is active, disconnecting..." >> ~/vpn-toggle.log
     nmcli connection down "$VPN_NAME"
     notify-send "VPN" "Отключен" -t 2000
 else
-    echo "$(date): VPN is inactive, connecting..." >> ~/vpn-toggle.log
     nmcli connection up "$VPN_NAME"
     notify-send "VPN" "Подключен" -t 2000
 fi
