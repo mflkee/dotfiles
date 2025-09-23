dc049ac8685aeb420d469985fd9ff65692b24737
mflkee <makeevgleb86rus@gmail.com>
2025-09-23 15:45:30 +0200

    add

--- dot_config/nvim/lua/mflkee/plugins/dadbod.lua
+++ dot_config/nvim/lua/mflkee/plugins/dadbod.lua
@@ -1,6 +1,13 @@
--- Dad Bod Plugins (минимальный сетап)
 return {
   "tpope/vim-dadbod",
   "kristijanhusak/vim-dadbod-completion",
   "kristijanhusak/vim-dadbod-ui",
+  config = function()
+    -- Явно отключаем автоматическое выполнение
+    vim.g.dadbod_auto_execute = 0
+    vim.g.db_ui_auto_execute = 0
+    
+    -- Отключаем возможные авто-действия completion
+    vim.g.vim_dadbod_completion_auto_execute = 0
+  end,
 }
