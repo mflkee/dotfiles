6463b77dbb4d3bb149677319ec9658550ffcb4cc
mflkee <makeevgleb86rus@gmail.com>
2025-09-24 04:36:37 +0200

    upd

--- dot_config/nvim/lua/mflkee/plugins/auto-save.lua
+++ dot_config/nvim/lua/mflkee/plugins/auto-save.lua
@@ -5,13 +5,31 @@ return {
       require("auto-save").setup({
         enabled = true,
         trigger_events = { "InsertLeave", "TextChanged", "TextChangedI" },
-        debounce_delay = 250, -- Обязательно добавьте это
+        debounce_delay = 250,
         condition = function(buf)
-          return not vim.fn.expand("%"):match("NvimTree") -- Игнорировать NvimTree
+          local fn = vim.fn
+          
+          -- Игнорировать NvimTree
+          if fn.expand("%"):match("NvimTree") then
+            return false
+          end
+          
+          -- Игнорировать SQL файлы
+          if vim.bo[buf].filetype:match("sql") then
+            return false
+          end
+          
+          -- Игнорировать временные файлы (простая проверка)
+          local buftype = fn.getbufvar(buf, "&buftype")
+          if buftype ~= "" and buftype ~= "acwrite" then
+            return false
+          end
+          
+          return true
         end,
         write_all_buffers = false,
         noautocmd = false,
-        silent = true, -- Отключить сообщения
+        silent = true,
       })
     end,
   }
