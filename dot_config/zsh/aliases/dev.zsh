4b67d2fe2f983bbbae6061cdf85e33c40a6e31cb
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-03-02 18:31:38 +0500

    upd zsh

--- dot_config/zsh/aliases/dev.zsh
+++ dot_config/zsh/aliases/dev.zsh
@@ -3,3 +3,5 @@ alias g++="g++ -std=c++23"
 alias rr="ranger"
 alias lg="lazygit"
 alias vim="nvim"
+
+alias hfga='hurl --resolve fgis.gost.ru:443:212.164.138.19 --connect-timeout 10 -m 25 --retry 0 --variables-file /home/mflkee/project/metrologenerator/tests/hurl/arshin.vars'
