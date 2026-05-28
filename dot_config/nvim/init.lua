3c98a64b1cc44aab4af4e3606ade8b2e6b84c14b
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-05-29 00:41:18 +0900

    switch system font to FiraCode Nerd Font Mono; drop qt6ct template

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
-vim.opt.guifont = "Iosevka Nerd Font Mono:h12"
+vim.opt.guifont = "FiraCode Nerd Font Mono:h12"
 
 vim.cmd("filetype plugin indent on")
 
