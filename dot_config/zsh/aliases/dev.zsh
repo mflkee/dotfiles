743f30b43714a1bed91520afdb7ee6835f1baefa
mflkee <makeevgleb86rus@gmail.com>
2026-07-23 16:25:33 +0500

    sync: archlinux-desktop 2026-07-23 16:25

--- dot_config/zsh/aliases/dev.zsh
+++ dot_config/zsh/aliases/dev.zsh
@@ -1,9 +1,10 @@
 # Development
-alias g++="g++ -std=c++23"
-alias rr="yazi"
-alias fm="yazi"
-alias files="yazi"
-alias lg="lazygit"
-alias vim="nvim"
+alias g++='g++ -std=c++23'
+alias rr='yazi'
+alias fm='yazi'
+alias files='yazi'
+alias lg='lazygit'
+alias vim='nvim'
 
 alias hfga='hurl --resolve fgis.gost.ru:443:212.164.138.19 --connect-timeout 10 -m 25 --retry 0 --variables-file /home/mflkee/project/metrologenerator/tests/hurl/arshin.vars'
+alias oc='opencode'
