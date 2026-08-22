7d6fbb2928c8b21aa905712e508421fce037da8f
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-08-22 22:54:28 +0500

    project sync: dotfiles

--- dot_config/zen-theme/user.js
+++ dot_config/zen-theme/user.js
@@ -2,3 +2,5 @@ user_pref("devtools.chrome.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("zen.widget.linux.transparency", true);
+/* dark accent so there is no default-orange flash before userChrome loads */
+user_pref("zen.theme.accent-color", "#0b0e14");
