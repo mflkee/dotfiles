c213ada445e40ba82fa2785920a706a3a5804f2b
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-08-22 22:39:25 +0500

    project sync: dotfiles

--- dot_config/zen-theme/user.js
+++ dot_config/zen-theme/user.js
@@ -1,2 +1,4 @@
 user_pref("devtools.chrome.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("zen.widget.linux.transparency", true);
