ff5fa295cc4854f590fb2e32338b0ef46468d539
mflkee <makeevgleb86rus@gmail.com>
2026-08-30 18:08:14 +0500

    project sync: dotfiles

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -5,3 +5,14 @@ require("yatline"):setup({
 	section_separator = { open = "", close = "" },
 	part_separator = { open = "", close = "" },
 })
+
+require("whoosh"):setup({
+	bookmarks = {
+		{ tag = "Projects",  key = "p", path = "~/projects/" },
+		{ tag = "Documents", key = "d", path = "~/Documents/" },
+		{ tag = "Obsidian",  key = "n", path = "~/obs_main/" },
+	},
+	jump_notify = false,
+	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	home_alias_enabled = true,
+})
