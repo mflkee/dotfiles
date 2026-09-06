54dd4adbd5f65f1bee3a2477ad6daf92a8fe585d
mflkee <makeevgleb86rus@gmail.com>
2026-09-06 20:24:10 +0500

    nvim: full replace — clean kickstart, remove leftover LazyVim dirs

--- dot_config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- mapleader defaults to " " in LazyVim.
-
--- Spell check English + Russian (incl. ё), so Russian words aren't flagged red.
--- Spell files: ~/.local/share/nvim/site/spell/ru.utf-8.{spl,sug}
-vim.opt.spelllang = { 'en', 'ru_yo' }
