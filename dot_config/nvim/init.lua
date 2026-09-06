af8989a9566404126264f49d9b8a86756807b184
mflkee <makeevgleb86rus@gmail.com>
2026-09-06 21:46:28 +0500

    dot-autosync: .config/nvim/init.lua (06.09 21:46)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -842,9 +842,10 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
-      -- rust = { 'rustfmt' },
+      rust = { 'rustfmt' },
+      lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
