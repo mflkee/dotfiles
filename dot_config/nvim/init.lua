98f46cf7185f7d398a61fa393e5c04da8f5172d7
mflkee <makeevgleb86rus@gmail.com>
2026-09-06 21:45:27 +0500

    dot-autosync: .config/nvim/init.lua (06.09 21:45)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -827,8 +827,9 @@ do
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
+        lua = true,
+        python = true,
+        rust = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
