a17aa6fa0211ddfdddc61e269ae3979f873f6245
mflkee <makeevgleb86rus@gmail.com>
2026-09-10 22:36:37 +0500

    nvim: bufferline + S-h/S-l buffer nav + gd goto definition

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -807,6 +807,9 @@ do
       --  For example, in C this would take you to the header.
       map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+      -- Goto the definition of the symbol under the cursor
+      map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+
       -- The following two autocommands are used to highlight references of the
       -- word under your cursor when your cursor rests there for a little while.
       --    See `:help CursorHold` for information about when this is executed
